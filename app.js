f7cb5db969431b89ec33d518ed1c36baacaaa511
Chantal Ramos <ramoscha@oregonstate.edu>
2022-11-28 14:50:57 -0800

    All functions working

--- app.js
+++ app.js
@@ -5,45 +5,55 @@ var express = require('express');
 var app = express();
 PORT = 3000;
 let {PythonShell} = require('python-shell');
-toTxt = "hola";
-fromTxt = "hello"
+bodyParser = require('body-parser');
 
 
 app.use(express.static('frontend'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json' }));
+
 /*
     ROUTES
 */
 
-
-app.get('/', function(req, res)
-    {res.render('index')
+//render the main page
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/" + "index.html")
     });
 
 
-function microservice(toText, fromText){
-    //runs the microservice python files that get the word count
-    let options = {
-        mode: 'text',
-        pythonOptions: ['-u'],                      // print results in real-time
-        args: [toText, fromText]}                   // pass translation as arguments to the translate.py microservice
+//receive toTxt and fromTxt data from text boxes and send to the microservice
+app.post('/', function (req, res) {
+    let fromText = req.body.fromTxt;
+    let toText = req.body.toTxt;
+    microservice(toText, fromText)
 
-    //start to run translate.py 
-    PythonShell.run('frontend/microservice/translate.py', options, function (err, results) {
-        if (err) throw err;
-    // results is an array consisting of messages collected during execution
-        console.log(results)
+    function microservice(fromText, toText){
+    //runs the microservice python files that get the word count
+        let options = {
+            mode: 'text',
+            pythonOptions: ['-u'],                      // print results in real-time
+            args: [fromText, toText]}                   // pass translation as arguments to the translate.py microservice
+    
+        //start to run translate.py 
+        PythonShell.run('frontend/microservice/translate.py', options, function (err, results) {
+            if (err) throw err;
+        // results is an array consisting of messages collected during execution
+            console.log(results)
+    
+    });
+        //start to run CCA.py
+        PythonShell.run('frontend/microservice/CCA.py', null, function (err, results) {
+            if (err) throw err;
+        // results is an array consisting of messages collected during execution
+            console.log(results)
+        
+    }); 
+    }
 
 });
-    //start to run CCA.py
-    PythonShell.run('frontend/microservice/CCA.py', null, function (err, results) {
-        if (err) throw err;
-    // results is an array consisting of messages collected during execution
-        console.log(results)
-    
-}); 
-}
 
-microservice(toTxt, fromTxt)
 
 
 /*
